58aa13ed085c5d4296a88a96dace0d0593048ce9
sysbr34kr <sysbr34kr@gmail.com>
2025-08-04 08:22:50 -0300

    P: Reverted changes from scan.sh
    This reverts commit 84881b3e06381b2004a38cd8f2f06cd4f315516c.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,4 +1,1 @@
-printf "\n========= SCANNING FOR PHONE NUMBERS =========\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "========= PHONE NUMBER SCAN COMPLETE ========="
+# TODO: write the script
